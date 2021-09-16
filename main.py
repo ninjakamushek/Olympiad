215c7970c5ff5ee3de0cda08051d0eb9e1b86566
Opelsine <nurbek.kenzhekhanov@astana.fizmat.kz>
2021-09-17 00:56:21 +0600

    r

--- main.py
+++ main.py
@@ -30,14 +30,8 @@ def index():
         session.commit()
     return render_template('index.html', title='Регистрация', form=form)
 
-def run_local_remote_available():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-
 def main():
     db_session.global_init("db/Olympiad.sqlite")
-    run_local_remote_available()
 
 if __name__ == '__main__':
     main()
